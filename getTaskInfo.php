4fcf94f67960075ba77895c3805447ab59f78ec1
JinXuyang <jinxuyang3@gmail.com>
2020-05-19 12:10:28 +0800

    增加了巨详细的注释

--- getTaskInfo.php
+++ getTaskInfo.php
@@ -1,70 +1,80 @@
 <?php
     header('Access-Control-Allow-Origin:*');
-    $host = "localhost";
+    //这一部分是用mysqli这个对象链接数据库
+    $host = "localhost";//数据库的地址
     $username = "root";
-    $dbname = "tasks";
+    $dbname = "tasks";//要连接的数据库名
     $passwd = "";
     $port = 3306;
-    $mysqli = new mysqli($host,$username,$passwd,$dbname,$port);
+    $mysqli = new mysqli($host,$username,$passwd,$dbname,$port);//new 一个mysqli对象
     
+    //判断链接是否成功
     if ($mysqli->connect_errno) {
         echo "Connect failed:", $mysqli->connect_error;
         exit();
     }
     
+    //使用$_GET获取客户端传上来的taskId
     $taskId = $_GET["taskId"];
 
-    
-        class taskInfo 
+    //创建一个对象，包含这个任务的各种信息
+    class taskInfo 
+    {
+        public $id;
+        public $name;
+        public $des;
+        public $type;
+        public $stime;
+        public $etime;
+        public $owner;
+        public $sta;
+        public $major;
+        public $class;
+        public $undone;
+        public $total;
+    }
+
+    //判断用户要查那个任务的信息，然后动态改变查询语句
+    if($taskId === '*'){
+        $gettaskinfo = "SELECT * FROM tasks_info";
+    }else{
+        $gettaskinfo = "SELECT * FROM tasks_info WHERE tasks_id = $taskId";
+    }
+
+    //执行语句，返回一个对象
+    $info = $mysqli->query($gettaskinfo);
+
+    //判读$info是否为空，空则没查找到或查找失败
+    if($info){
+        //遍历$info的每一行
+        while ($row = $info->fetch_row())
         {
-            public $id;
-            public $name;
-            public $des;
-            public $type;
-            public $stime;
-            public $etime;
-            public $owner;
-            public $sta;
-            public $major;
-            public $class;
-            public $undone;
-            public $total;
+
+            $taskInfo = new taskInfo();//new 一个taskInfo对象
+            //写入对应信息
+            $taskInfo->id = $row[0];
+            $taskInfo->name = $row[1];
+            $taskInfo->des = $row[2];
+            $taskInfo->type = $row[3];
+            $taskInfo->stime = $row[4];
+            $taskInfo->etime = $row[5];
+            $taskInfo->owner = $row[6];
+            $taskInfo->sta = $row[7];
+            $taskInfo->major = $row[8];
+            $taskInfo->class = $row[9];
+            $taskInfo->undone = $row[10];
+            $taskInfo->total = $row[11];
+
+            //新建一个data数组，存放每一行的信息
+            $data[]=$taskInfo;
+
         }
-        
-        if($taskId === '*'){
-            $gettaskinfo = "SELECT * FROM tasks_info";
-        }else{
-            $gettaskinfo = "SELECT * FROM tasks_info WHERE tasks_id = $taskId";
+        //
+        $json = json_encode($data);//把数据转换为JSON数据.
+        echo $json;//向客户端返回json数据
+
+    }else{
+        echo "查询失败";
         }
-
-        $info = $mysqli->query($gettaskinfo);
-
-        if($info){
-            while ($row = $info->fetch_row())
-            {
-                $taskInfo = new taskInfo();
-                $taskInfo->id = $row[0];
-                $taskInfo->name = $row[1];
-                $taskInfo->des = $row[2];
-                $taskInfo->type = $row[3];
-                $taskInfo->stime = $row[4];
-                $taskInfo->etime = $row[5];
-                $taskInfo->owner = $row[6];
-                $taskInfo->sta = $row[7];
-                $taskInfo->major = $row[8];
-                $taskInfo->class = $row[9];
-                $taskInfo->undone = $row[10];
-                $taskInfo->total = $row[11];
-
-                $data[]=$taskInfo;
-
-            }
-        
-            $json = json_encode($data);//把数据转换为JSON数据.
-            echo $json;
-
-        }else{
-            echo "查询失败";
-            }
-        $mysqli->close();
-    ?>
+    $mysqli->close();//关闭与数据库的连接
+?>
--- js/index.js
+++ js/index.js
@@ -1,36 +1,38 @@
 
-
+//这个函数的功能是，向服务器发送get请求，服务器返回一个json数据，内容包括任务名称开始时间，结束时间.....
 function gettaskinfo(){
     var jsondata;
+    //这个$.ajax是jquery的一个方法，比原生js好用，作用是发送Ajax请求的
     $.ajax({
-            url:'http://121.36.19.47/tasks/getTaskInfo.php',
-            type:'get',
-            async: false,
-            data:{
+            url:'http://121.36.19.47/tasks/getTaskInfo.php',//请求的地址
+            type:'get',//请求类型
+            async: false,//同步or异步，false为同步
+            data:{//向服务器发送的数据
                 taskId:'*',
             },
-            success:function(data){ 
+            success:function(data){ //成功后执行，data为服务器返回的东西
                 console.log("ok");
-                jsondata = $.parseJSON(data);
+                jsondata = $.parseJSON(data);//这个$.parseJSON(),用来解析json数据，把json数据变成一个js对象
             },
-            error:function(errorinfo){
+            error:function(errorinfo){//错误后执行
                 console.log("错误"+errorinfo.statusText);
             }
     })
-    return jsondata;
+    return jsondata;//把这个js对象return出去
 }
-
+//这个函数的功能是，任务的相关信息插入到div2这个地方
 function showlist(){
     var jsoninfo = gettaskinfo();
     
-    for (let index = 0; jsoninfo[index] != undefined; index++) {
-        var percent = (jsoninfo[index].total-jsoninfo[index].undone)/jsoninfo[index].total;
+    for (let index = 0; jsoninfo[index] != undefined; index++) {//遍历jsoninfo(有点像php里的关联数组?)这个对象的所有小伙子
+        var percent = (jsoninfo[index].total-jsoninfo[index].undone)/jsoninfo[index].total;//计算完成人数的百分比
+        //一会就是把这个对象插到div2里
         var task = '<a href="http://121.36.19.47/tasks/detail.html?task_id='+ jsoninfo[index].id +' " class="list-group-item list-group-item-action" ><div ><h5 class="mb-1">' + jsoninfo[index].name + '</h5><small>创建时间:'+jsoninfo[index].stime+'</small></div><p class="mb-1">'+jsoninfo[index].des+'</p><div class="progress" style="height: 13px;"><div class="progress-bar" role="progressbar"  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: ' + percent +'%;">'+ percent+'%</div></div></a>';
         $("#div2").append(task);
     }
 }
 
-
+//页面加载完后执行
 window.onload = function () {
     showlist();
 }
